5b09379d91d2728b0f3792213b20adf000132160
jupiterbjy <jupiterbjy@gmail.com>
2021-01-09 04:08:33 +0900

    Added patch to support callbacks on widgets that doesn't. First bare-minimum working state. Will now separate from Incubator.

--- PlayAudioFile.py
+++ PlayAudioFile.py
@@ -1,16 +1,21 @@
 import sounddevice as sd
 import soundfile as sf
 import py_cui
+import functools
 from collections import OrderedDict
 from tinytag import TinyTag
 from os import listdir
 from os.path import abspath, dirname, join
 from numpy import ndarray
+from typing import Callable
+
+import pretty_errors
+pretty_errors.activate()
 
 
 # Assuming relative location
-AUDIO_FOLDER = "files"
-AUDIO_TYPES = ".ogg", ".mp3", ".m4a"
+AUDIO_FOLDER = "audio_files"
+AUDIO_TYPES = ".ogg", ".mp3", ".m4a", ".flac"
 
 
 class Device:
@@ -33,8 +38,9 @@ def stop_track():  # She's so smol
 
 
 def extract_metadata(abs_file_dir):
-    tag = TinyTag.get(abs_file_dir)  # dirty trick
-    return OrderedDict(sorted(tag.as_dict().items()))
+    tag = TinyTag.get(abs_file_dir)
+    filtered = sorted(((k, v) for k, v in tag.as_dict().items() if v))
+    return OrderedDict(filtered)
 
 
 def fetch_files():
@@ -50,36 +56,57 @@ def fetch_files():
         return [fn for fn in file_list if fn.endswith(AUDIO_TYPES)]
         # I love this shiny new suffix / prefix search. Only in 3.9+.
 
+
+def add_callback_patch(widget_: py_cui.widgets.Widget, callback: Callable) -> None:
+    """
+    Adding callback support for widget that lacks such as ScrollMenu.
+
+    :param widget_: Any widget you want to add callback on each update.
+    :param callback: Any callables
+    :return: None
+    """
+    # Seems like sequence is _draw -> _handle_mouse_press, so patching on _draw results 1 update behind.
+    # Therefore we need to patch both _handle_mouse_press and _handle_keyboard_press first.
+
+    def patch_factory(old_func):
+        # fix for late binding issue: stackoverflow.com/questions/3431676
+        @functools.wraps(old_func)
+        def wrapper(*args, **kwargs):
+            old_func(*args, **kwargs)
+            callback()
+
+        return wrapper
+
+    for target in ("_handle_key_press", "_handle_mouse_press"):
+        setattr(widget_, target, patch_factory(getattr(widget_, target)))
+
+
 # ------------------------------------------------------------------
 # UI definition, using py-cui examples. Would've been nice if it followed PEP8.
 
 
 class AudioPlayer:
-    ellipsis_ = ".."  # 3 dots 2 long
+    ellips = ".."  # 3 dots 2 long
     # EXPECTING 5, 3 Layout!
 
     def __init__(self, root: py_cui.PyCUI):
         self.root_ = root
 
-        # Doing a trick, putting button and scroll view at same position,
-        # then update scroll menu to re-draw over button.
         self.audio_list = self.root_.add_scroll_menu("Files", 0, 0, column_span=3, row_span=3)
-        # self.hidden_btn = self.root_.add_button("U Nya", 0, 0, column_span=3, row_span=3, command=self.update_meta)
-
         self.meta_list = self.root_.add_scroll_menu("Meta", 0, 3, column_span=2, row_span=5)
         self.progress_blk = self.root_.add_text_box("Info", 3, 0, column_span=3)
         self.play_btn = self.root_.add_button("Play", 4, 0, command=self.play_cb)
         self.stop_btn = self.root_.add_button("Stop", 4, 1, command=self.stop_cb)
         self.reload_btn = self.root_.add_button("Reload", 4, 2, command=self.reload_cb)
-
-        # self.hidden_btn.is_selectable = False
-
+        self.audio_list.get
         # just for ease of clearing
         self.clear_target = [self.audio_list, self.meta_list, self.progress_blk]
 
-        # Key-binding space bar to play. No, no toggle yet.
+        # add callback to update metadata on every redraw.
+        add_callback_patch(self.audio_list, self.update_meta)
+
+        # Key binds
         self.play_btn.add_key_command(py_cui.keys.KEY_SPACE, self.play_cb)
-        self.meta_list.add_key_command(py_cui.keys.KEY_ENTER, self.wrapper)
 
         self.files = []
         self.playing = False
@@ -91,7 +118,10 @@ class AudioPlayer:
             self.stop_cb()
 
         self.playing = True
-        play_track(self.abs_dir(self.current))
+        try:
+            play_track(self.abs_dir(self.current))
+        except IndexError:
+            self.playing = False
 
     def stop_cb(self):
         self.playing = False
@@ -101,10 +131,7 @@ class AudioPlayer:
     def reload_cb(self):
         # clear widgets
         for widget in self.clear_target:
-            try:
-                widget.clear()
-            except AttributeError:
-                print(f"{type(widget)} don't have clear()!")
+            widget.clear()
 
         # fetch new file list
         self.files = fetch_files()
@@ -116,10 +143,6 @@ class AudioPlayer:
 
         self.update_meta()
 
-    def wrapper(self):
-        self.progress_blk.set_text("a")
-        return self.update_meta()
-
     # TODO: add exception handling later
     # TODO: fetch metadata area's physical size and put ellipsis accordingly.
     def update_meta(self):
@@ -127,22 +150,19 @@ class AudioPlayer:
         self.meta_list.clear()
 
         # Extract metadata
-        ordered = extract_metadata(self.abs_dir(self.current))
+        try:
+            ordered = extract_metadata(self.abs_dir(self.current))
+        except IndexError:
+            return
 
         # calculate average key length - in case it's too long. Maybe violation of KISS.
-        key_average = sum(map(len, ordered.keys())) // len(ordered)
+        key_avg = sum(map(len, ordered.keys())) // len(ordered)
 
         for key, val in ((k, v) for k, v in ordered.items() if v):
-            formatted = key[::key_average - len(self.ellipsis_)] + self.ellipsis_
-            self.meta_list.add_item(f"{formatted.ljust(key_average)}: {val}")
+            formatted = key if len(key) <= key_avg else key[::key_avg - len(self.ellips)] + self.ellips
+            self.meta_list.add_item(f"{formatted.ljust(key_avg)}: {val}")
 
         # redraw scroll_view
-        self.update_widget(self.audio_list)
-
-    @staticmethod
-    def update_widget(target):
-        # updating target to re-draw over overlapping widgets.
-        target.set_border_color(target.get_border_color())
 
     @property
     def current(self):
