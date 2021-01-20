3fb6b963e43512ffaeca475da28b513dbb3fd49a
jupiterbjy <jupiterbjy@gmail.com>
2021-01-21 03:24:17 +0900

    Implemented Class based stream manager. Not applied to player yet.

--- SoundModule.py
+++ SoundModule.py
@@ -1,8 +1,9 @@
 import sounddevice as sd
 import soundfile as sf
 import threading
-from tinytag import TinyTag
-from typing import Callable, Tuple
+import itertools
+from tinytag import TinyTag, Ogg, ID3, Flac, Wave
+from typing import Callable, Tuple, Union
 
 from LoggingConfigurator import logger
 
@@ -91,8 +92,8 @@ def play_audio_not_safe(
 
     def finish_cb() -> None:
         logger.debug("Playback finished.")
-        finished_callback()
         audio_fp.close()
+        finished_callback()
 
     audio_fp = sf.SoundFile(audio_location)
 
@@ -138,5 +139,102 @@ def play_audio_not_safe(
     return audio_fp, stream
 
 
-def stop_audio():
-    sd.stop()
+class StreamManager:
+    def __init__(self, stream_callback: Callable = None, finished_callback: Callable = None, callback_every_n=3):
+        self.audio_dir = ""
+        self.callback_minimum_cycle = callback_every_n
+
+        self.stream_cb = stream_callback if stream_callback else lambda x, y, z: None
+        self.finished_cb = finished_callback if stream_callback else lambda: None
+
+        self.audio_data: sf.SoundFile = None
+        self.tag_data: Union[TinyTag, ID3, Ogg, Wave, Flac] = None
+
+        self.total_frame: int = None
+        self.duration_tag: int = None
+
+        self.stream: sd.OutputStream = None
+        self.abort = False
+        self.paused = True
+
+    def stream_callback_closure(self) -> Callable:
+        # Collecting names here to reduce call overhead.
+        last_frame = -1
+        dtype = sd.default.dtype[1]
+        audio_ref = self.audio_data
+        callback = self.stream_cb
+        duration = self.duration_tag
+
+        cycle = itertools.cycle((not n for n in range(self.callback_minimum_cycle)))
+        # this looks much neat than changing variable inside callback.
+
+        def stream_cb(data_out, frames: int, time, status: sd.CallbackFlags) -> None:
+            nonlocal last_frame
+
+            assert not status
+
+            # changing order would results in micro bit faster runs, but IDE complains so..
+            if last_frame == (current_frame := audio_ref.tell()) or self.abort:
+                raise sd.CallbackStop
+
+            audio_ref.buffer_read_into(data_out, dtype)
+            if next(cycle):
+                callback(audio_ref, current_frame, duration)
+            # Stream callback signature for other functions
+
+        return stream_cb
+
+    def finished_callback_wrapper(self):
+        logger.debug(f"Playback finished. State:{'Abort' if self.abort else 'Finished'}")
+        self.audio_data.close()
+        if not self.abort:
+            self.finished_cb()
+
+    def load_new_stream(self, audio_location):
+        self.audio_dir = audio_location
+
+        self.audio_data = sf.SoundFile(self.audio_dir)
+        self.tag_data = TinyTag.get(self.audio_dir)
+
+        self.total_frame = self.audio_data.frames
+        self.duration_tag = round(self.tag_data.duration)
+
+        self.abort = False
+
+        self.stream = sd.OutputStream(
+            samplerate=self.audio_data.samplerate,
+            channels=self.audio_data.channels,
+            callback=self.stream_callback_closure(),
+            finished_callback=self.finished_cb
+        )
+
+    def start_stream(self):
+        self.stream.start()
+
+    def stop_stream(self):
+        self.stream.stop()
+
+    def pause_stream(self):
+        if self.paused:
+            self.start_stream()
+        else:
+            self.stop_stream()
+
+        self.paused = not self.paused
+
+    def __del__(self):
+        self.stream.stop()
+        self.audio_data.close()
+
+
+if __name__ == '__main__':
+    def test():
+        """
+        >>> from SoundModule import StreamManager
+        >>> audio_location_ = r"E:\github\CUIAudioPlayer\audio_files\Higher's High   ナナヲアカリ.ogg"
+        >>> ref = StreamManager()
+        >>> ref.load_new_stream(audio_location_)
+        >>> ref.start_stream()
+        """
+
+    # Try this in python console. of course change the audio.
